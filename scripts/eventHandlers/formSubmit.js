4a9a15dcb7aa7c0eb7be72ffddbc29938460bc04
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-04-01 14:36:43 +0530

    feat: edit via query params by passing productId

--- scripts/eventHandlers/formSubmit.js
+++ scripts/eventHandlers/formSubmit.js
@@ -1,7 +1,22 @@
+import getProducts from "../getProducts.js";
 import setProduct from "../services/setProduct.js";
 
 export default function formSubmit() {
     let form = document.getElementById("product-form");
+    let productId = window.location.search.slice(11);
+    if (productId.length) {
+        let products = getProducts();
+        if (!products[productId]) alert("Product Doesn't exist");
+        else {
+            let product = products[productId];
+            let inputs = form.children;
+            for (let elem of inputs) {
+                if (elem.classList[0] !== "form-input") continue;
+                elem.value = product[elem.name];
+            }
+            console.log(inputs.value);
+        }
+    }
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         let formData = new FormData(e.target);
